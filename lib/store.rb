ca9ec2c06271c415c600a1221ec47fe35af64da2
Reflekshn <reflekshn@hotmail.com>
2022-02-05 21:11:07 -0500

    Add validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates_associated :employees
+  validates :name, prescence: true, length:{minimum: 3}
+  validates :annual_revenue numericality:{only_integer: true, greater_than: 0}
+  validate :men_or_womens_apparel
 
+  def men_or_womens_apparel
+    if !self.womens_apparel && !self.mens_apparel
+      errors.add :men_or_womens_apparel, message:"Must sell either mens or womens apparel or both"
+    end
+  end
 end
